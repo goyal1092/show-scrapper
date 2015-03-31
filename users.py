f7227af6b88d344624e4ed3c6e175e36d32829fe
Gaurav Goyal <gaurav.goyal@openlabs.co.in>
2015-03-31 08:41:49 +0530

    updating show dict

--- users.py
+++ users.py
@@ -3,6 +3,17 @@ show_dict = {
     "Game_of_Thrones": "http://www.imdb.com/title/tt0944947/?ref_=nv_sr_1",
     "Arrow":"http://www.imdb.com/title/tt2193021/?ref_=fn_al_tt_1",
     "Gotham": "http://www.imdb.com/title/tt3749900/?ref_=fn_al_tt_1",
+    "Better_Call_Saul": "http://www.imdb.com/title/tt3032476/",
+    "the_big_bang_theory": "http://www.imdb.com/title/tt0898266/",
+    "supernatural": "http://www.imdb.com/title/tt0460681/",
+    "agents_of_shield": "http://www.imdb.com/title/tt2364582/",
+    "suits": "http://www.imdb.com/title/tt1632701/",
+    "sherlock": "http://www.imdb.com/title/tt1475582/",
+    "castle": "http://www.imdb.com/title/tt1219024/",
+    "homeland": "http://www.imdb.com/title/tt1796960/",
+    "person_of_intrest": "http://www.imdb.com/title/tt1839578/",
+    "grimm": "http://www.imdb.com/title/tt1830617/",
+    "agent_carter": "http://www.imdb.com/title/tt3475734/"
 }
 
 users = [{
